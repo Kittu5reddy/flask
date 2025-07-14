0948b2638f7e0ee4382975b3498604b5fa7ef050
kittu5reddy <kaushikpalvai2004@gmail.com>
2025-07-14 22:02:47 +0530

    day-1

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("hello world")
+from flask import Flask,render_template
+
+app=Flask(__name__)#__name__
+
+@app.route('/home')
+def homepage():
+    return render_template("home.html")
+
+@app.route('/about')
+def aboutpage():
+    return render_template("about.html")
+
+
+
+if __name__=="__main__":
+    app.run(port=9000,debug=True)
